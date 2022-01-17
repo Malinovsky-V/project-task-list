6f12c994cfb49cef103ce3390ac4f263fae27f81
Victor <mv.vitya@gmail.com>
2022-01-17 14:45:47 +0200

    Updates

--- src/components/UI/input/MyInput.jsx
+++
@@ -1,9 +0,0 @@
-import classes from './MyInput.module.css'
-
-export default function MyInput(props) {
-    return (
-        <input className={`${classes.myInput} ${props.emptyField}`} {...props}>
-            
-        </input >
-    )
-}
